fb9c450f493ad42ff869efc7b2dec2c8a9de7e04
amro212 <amromousa8@gmail.com>
2025-09-06 02:04:16 -0400

    Initial Commit

---
+++ Syllabus Sync/Syllabus_SyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  Syllabus_SyncApp.swift
+//  Syllabus Sync
+//
+//  Created by Amro Zabin on 2025-09-06.
+//
+
+import SwiftUI
+
+@main
+struct Syllabus_SyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
